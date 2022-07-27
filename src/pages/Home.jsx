49a7114c78653d1316afeabd874faaea83ce1201
Eliott Abadie (laptop) <eliott.abadiesouchar@icloud.com>
2022-07-27 13:08:24 +0200

    mui date picker finally installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,41 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import { Header } from "../components/Header"
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { states } from "../states";
 
 export function Home(){
+
+    const [selectedDate, handleDateChange] = useState(new Date());
 
     return(
         <Fragment>
             <Header/>
             <main>
                 <form>
-                    <label htmlFor="firstname">First Name</label>
-                    <input type="text" id="firstname"/>
-                    <label htmlFor="lastname">Last Name</label>
-                    <input type="text" id="lastname"/>
-                    <label htmlFor="birthdate">Date of Birth</label>
-                    <input type="text" id="birthdate"/>
-                    <label htmlFor="startdate">Start Date</label>
-                    <input type="text" id="startdate"/>
-                    <fieldset>
-                        <legend>Address</legend>
-                        <label htmlFor="street">Street</label>
-                        <input type="text" id="street"/>
-                        <label htmlFor="city">City</label>
-                        <input type="text" id="city"/>
-                    </fieldset>
+                    <TextField id="standard-basic" label="First Name" variant="standard" />
+                    <TextField id="standard-basic" label="Last Name" variant="standard" />
+                    <TextField id="standard-basic" type="date" label="Date of Birth" variant="standard" />
+                    <LocalizationProvider dateAdapter={AdapterMoment}>
+                        <DatePicker
+                            label="Basic example"
+                            value={selectedDate}
+                            onChange={(newValue) => {
+                            handleDateChange(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={states}
+                        sx={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="State" />}
+                    />
                 </form>
             </main>
         </Fragment>
