b7c7512c3a159a8801a14ae158d2e6bcfaa48bfa
Eliott Abadie (laptop) <eliott.abadiesouchar@icloud.com>
2022-08-06 15:22:02 +0200

    input handled + mui optimization

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,16 @@
+import { Fragment } from "react";
+import BACKGROUND from "../assets/background.jpg"
+import "../css/pages/employees.css"
+
 export function Employees(){
 
     return(
-        <div>zee</div>
+        <Fragment>
+            <img src={BACKGROUND} alt="" className="backgroundImage"/>
+            <main className="employeesmain">
+                
+            </main>
+        </Fragment>
     )
 
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { Fragment, useState } from "react"
+import React from "react";
+import { Fragment, useState, useMemo } from "react"
 import { Logo } from "../components/Logo"
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
@@ -10,19 +11,94 @@ import "../css/pages/home.css"
 import BACKGROUND from "../assets/background.jpg"
 import { Header } from "../components/Header";
 
-export function Home(){
+function PureComponent(props){
+    const pureInput = useMemo(()=>{
+        return(
+            props.component
+        )
+    }, [props.listen])
+    return pureInput
+}
 
-    const [startdate, changeStartDate] = useState(new Date());
-    const [birthdate, changeBirthDate] = useState(new Date());
+export const Home = React.memo(()=> {
+
+    
     const [firstName, changeFirstName] = useState("")
-    const [firstNameError, setFirstNameError] = useState({error: false, message: ""})
+    const [firstNameError, setFirstNameError] = useState({error: false, message: " "})
 
-    function handleInput(change, value, setError){
+    const [lastName, changeLastName] = useState("")
+    const [lastNameError, setLastNameError] = useState({error: false, message: " "})
+    
+    const [birthdate, changeBirthDate] = useState(new Date());
+    const [birthdateError, setBirthDateError] = useState({error: false, message: " "})
+    
+    const [startdate, changeStartDate] = useState(new Date());
+    const [startdateError, setStartDateError] = useState({error: false, message: " "})
+    
+    const [city, changeCity] = useState("");
+    const [cityError, setCityError] = useState({error: false, message: " "})
+    
+    const [street, changeStreet] = useState("");
+    const [streetError, setStreetError] = useState({error: false, message: " "})
+
+    const [state, changeState] = useState("");
+    const [stateError, setStateError] = useState({error: false, message: " "})
+
+    const [zipcode, changeZipcode] = useState("");
+    const [zipcodeError, setZipcodeError] = useState({error: false, message: " "})
+
+    const [department, changeDepartment] = useState("");
+    const [departmentError, setDepartmentError] = useState({error: false, message: " "})
+    
+    function handleInput(change, value, setError, rules){
         change(value)
-        if (value.length < 3) {
-            setError({error: true, message: "should have more than 2 letters"})
-        }else{
-            setError({error: false, message: ""})
+        let error = false
+        if (rules === "text") {
+            if (value.length > 0 && value.length < 3) {
+                setError({error: true, message: "Must have more than 2 letters"})
+                error = true
+            }
+            if(value.match(/[^A-Za-zÀ-ÿ-]/)){
+                setError({error: true, message: "Must only include letters"})
+                error = true
+            }
+            if(error === false){
+                setError({error: false, message: " "})
+            }
+        }
+
+        if (rules === "birthdate") {
+            if(value > new Date()){
+                setError({error: true, message: "Negative birth date"})
+            }else{
+                setError({error: false, message: " "})
+            }
+        }
+        
+        if (rules === "address") {
+            if(value.match(/[^A-Za-zÀ-ÿ- ]/)){
+                setError({error: true, message: "Bad character"})
+                error = true
+            }else{
+                setError({error: false, message: " "})
+            }
+        }
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        if (!
+            (firstNameError.error ||
+            lastNameError.error ||
+            cityError.error ||
+            streetError.error ||
+            stateError.error ||
+            zipcodeError.error ||
+            departmentError.error ||
+            birthdateError.error ||
+            startdateError.error)
+        ) {
+            console.log("ok")
         }
     }
 
@@ -30,91 +106,183 @@ export function Home(){
         <Fragment>
             <img src={BACKGROUND} alt="" className="backgroundImage"/>
             <Header page="home"/>
-            <main>
+            <main className="homemain">
                 <Logo></Logo>
-                <form>
-                    <TextField 
-                        id="standard-basic" 
-                        label="First Name" 
-                        variant="standard" 
-                        required
-                        error={firstNameError.error}
-                        helperText={firstNameError.message}
-                        value={firstName}
-                        onChange={(e) => handleInput(changeFirstName, e.target.value, setFirstNameError)}
+                <form onSubmit={(e)=>{handleSubmit(e)}}>
+                    <PureComponent
+                        component={
+                            <TextField 
+                                id="standard-basic" 
+                                label="First Name" 
+                                variant="standard" 
+                                required
+                                error={firstNameError.error}
+                                helperText={firstNameError.message}
+                                value={firstName}
+                                onChange={(e) => handleInput(changeFirstName, e.target.value, setFirstNameError, "text")}
+                            />
+                        }
+                        listen={firstName}
                     />
-                    <TextField 
-                        id="standard-basic" 
-                        label="Last Name" 
-                        variant="standard" 
-                        required
+                    <PureComponent
+                        component={
+                            <TextField 
+                                id="standard-basic" 
+                                label="Last Name" 
+                                variant="standard" 
+                                required
+                                error={lastNameError.error}
+                                helperText={lastNameError.message}
+                                value={lastName}
+                                onChange={(e) => handleInput(changeLastName, e.target.value, setLastNameError, "text")}
+                            />
+                        }
+                        listen={lastName}
                     />
                     <LocalizationProvider dateAdapter={AdapterMoment}>
-                        <DatePicker
-                            label="Date of Birth"
-                            value={birthdate}
-                            onChange={(newValue) => {
-                            changeBirthDate(newValue);
-                            }}
-                            renderInput={(params) => <TextField 
-                                {...params} 
-                                error 
-                                helperText="pl"
-                                required
-                            />}
+                        <PureComponent
+                            component={
+                                <DatePicker
+                                    label="Date of Birth"
+                                    value={birthdate}
+                                    onChange={(e) => {
+                                        handleInput(changeBirthDate, e, setBirthDateError, "birthdate")
+                                    }}
+                                    renderInput={(params) => <TextField 
+                                        {...params} 
+                                        error={birthdateError.error}
+                                        helperText={birthdateError.message}
+                                        required
+                                    />}
+                                />
+                            }
+                            listen={birthdate}
                         />
-                        <DatePicker
-                            label="Start Date"
-                            value={startdate}
-                            onChange={(newValue) => {
-                            changeStartDate(newValue);
-                            }}
-                            renderInput={(params) => <TextField 
-                                {...params} 
-                                required
-                            />}
+                        <PureComponent
+                            component={
+                                <DatePicker
+                                    label="Start Date"
+                                    value={startdate}
+                                    onChange={(e) => {
+                                        handleInput(changeStartDate, e, setStartDateError)
+                                    }}
+                                    renderInput={(params) => <TextField 
+                                        {...params} 
+                                        error={startdateError.error}
+                                        helperText={startdateError.message}
+                                        required
+                                    />}
+                                />
+                            }
+                            listen={startdate}
                         />
                     </LocalizationProvider>
                     <fieldset className="address">
                         <legend>Address</legend>
-                        <TextField 
-                            id="standard-basic" 
-                            label="City" 
-                            variant="standard" 
-                            required
+                        <PureComponent
+                            component={
+                                <TextField 
+                                    id="standard-basic" 
+                                    label="City" 
+                                    variant="standard"
+                                    error={cityError.error}
+                                    helperText={cityError.message}
+                                    required
+                                    value={city}
+                                    onChange={(e) => {
+                                        handleInput(changeCity, e.target.value, setCityError, "address")
+                                    }}
+                                />
+                            }
+                            listen={city}
                         />
-                        <TextField 
-                            id="standard-basic" 
-                            label="Street" 
-                            variant="standard" 
-                            required
+                        <PureComponent
+                            component={
+                                <TextField 
+                                    id="standard-basic" 
+                                    label="Street" 
+                                    variant="standard" 
+                                    error={streetError.error}
+                                    helperText={streetError.message}
+                                    required
+                                    value={street}
+                                    onChange={(e) => {
+                                        handleInput(changeStreet, e.target.value, setStreetError, "address")
+                                    }}
+                                />
+                            }
+                            listen={street}
                         />
-                        <Autocomplete
-                            disablePortal
-                            id="combo-box-demo"
-                            options={states}
-                            sx={{ width: 300 }}
-                            renderInput={(params) => <TextField {...params} label="State" required/>}
+                        <PureComponent
+                            component={
+                                <Autocomplete
+                                    disablePortal
+                                    id="combo-box-demo"
+                                    options={states}
+                                    sx={{ width: 300 }}
+                                    renderInput={(params) => <TextField 
+                                        {...params}
+                                        label="State"
+                                        required
+                                        error={stateError.error}
+                                        helperText={stateError.message}
+                                        value={state}
+                                        onChange={(e) => {
+                                            handleInput(changeState, e.target.value, setStateError)
+                                        }}
+                                        />
+                                    }
+                                />
+                            }
+                            listen={state}
                         />
-                        <TextField 
-                            id="standard-basic" 
-                            label="Zip code" 
-                            variant="standard" 
-                            required
-                            type="number"
+                        <PureComponent
+                            component={
+                                <TextField 
+                                    id="standard-basic" 
+                                    label="Zip code" 
+                                    variant="standard" 
+                                    type="number"
+                                    required
+                                    error={zipcodeError.error}
+                                    helperText={zipcodeError.message}
+                                    value={zipcode}
+                                    onChange={(e) => {
+                                        handleInput(changeZipcode, e.target.value, setZipcodeError)
+                                    }}
+                                />
+                            }
+                            listen={zipcode}
                         />
+
                     </fieldset>
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-demo"
-                        options={departments}
-                        sx={{ width: 300 }}
-                        renderInput={(params) => <TextField {...params} label="Department" required/>}
-                    />
+                    <PureComponent
+                            component={
+                                <Autocomplete
+                                    disablePortal
+                                    id="combo-box-demo"
+                                    options={departments}
+                                    sx={{ width: 300 }}
+                                    renderInput={(params) => <TextField 
+                                        {...params}
+                                        label="Department"
+                                        required
+                                        error={departmentError.error}
+                                        helperText={departmentError.message}
+                                        value={department}
+                                        onChange={(e) => {
+                                            handleInput(changeDepartment, e.target.value, setDepartmentError)
+                                        }}
+                                        />
+                                    }
+                                />
+                            }
+                            listen={department}
+                        />
                     <button className="savebutton">Save</button>
                 </form>
             </main>
         </Fragment>
     )
 
-}
+})
