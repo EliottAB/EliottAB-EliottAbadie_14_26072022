37b94b833487f119d5360c9878fce40432ee5d34
Abadie Eliott <eliott.abadiesouchar@icloud.com>
2022-08-11 11:46:27 +0200

    Able to select a max List + some css & responsive

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,5 @@
-import _ from "lodash"
-import React, { useState } from "react"
+import _, { lastIndexOf } from "lodash"
+import React, { useCallback, useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import "../css/components/table.css"
 import { TableRow } from "./TableRow"
@@ -9,46 +9,59 @@ export function Table(){
     const defaultSort = "firstName"
     const employees = useSelector(state => state.employees)
 
-    const [maxEmployees, setMaxEmployees] = useState(5)
+    const [maxEmployees, setMaxEmployees] = useState(10)
     const [page, setPage] = useState(0)
     const [sortedEmployees, setSortedEmployees] = useState(()=>{
         const baseValue = _.orderBy(employees, [defaultSort], ["asc"])
         return _.chunk(baseValue, maxEmployees)
     })
-
-    function sortEmployees(sort){
+    const [lastSort, setLastSort] = useState(defaultSort)
+    
+    const sortEmployees = useCallback((sort = lastSort)=>{
         setSortedEmployees(()=>{
-            const baseValue = _.orderBy(employees, [sort], ["asc"])
+            const baseValue = _.orderBy(employees, [sort ? sort : lastSort], ["asc"])
+            setLastSort(sort)
             return _.chunk(baseValue, maxEmployees)
         })
-    }
+    }, [employees, maxEmployees, lastSort, setSortedEmployees])
     
+    useEffect(()=>{
+        sortEmployees()
+    }, [maxEmployees, sortEmployees])
 
     return(
         <React.Fragment>
         {
             sortedEmployees[0] ?
-            <div className="tableContainer">
-                <table className="employeelist">
-                    <thead>
-                        <tr>
-                            <th><button onClick={()=>{sortEmployees("firstName")}}>First Name</button></th>
-                            <th><button onClick={()=>{sortEmployees("lastName")}}>Last Name</button></th>
-                            <th><button onClick={()=>{sortEmployees("startDate")}}>Start Date</button></th>
-                            <th><button onClick={()=>{sortEmployees("department")}}>Department</button></th>
-                            <th><button onClick={()=>{sortEmployees("birthDate")}}>Date of Birth</button></th>
-                            <th><button onClick={()=>{sortEmployees("street")}}>Street</button></th>
-                            <th><button onClick={()=>{sortEmployees("city")}}>City</button></th>
-                            <th><button onClick={()=>{sortEmployees("state")}}>State</button></th>
-                            <th><button onClick={()=>{sortEmployees("zip")}}>Zip Code</button></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {sortedEmployees[page].map((employee) => <TableRow employee={employee} key={sortedEmployees[page].indexOf(employee)}/>)}
-                    </tbody>
-                </table>
-            </div>
-         : null
+            <React.Fragment>
+                <select className="maxrows" onChange={(e)=>{setMaxEmployees(parseInt(e.target.value))}}>
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+                <div className="tableContainer">
+                    <table className="employeelist">
+                        <thead>
+                            <tr>
+                                <th><button onClick={()=>{sortEmployees("firstName")}}>First Name</button></th>
+                                <th><button onClick={()=>{sortEmployees("lastName")}}>Last Name</button></th>
+                                <th><button onClick={()=>{sortEmployees("startDate")}}>Start Date</button></th>
+                                <th><button onClick={()=>{sortEmployees("department")}}>Department</button></th>
+                                <th><button onClick={()=>{sortEmployees("birthDate")}}>Date of Birth</button></th>
+                                <th><button onClick={()=>{sortEmployees("street")}}>Street</button></th>
+                                <th><button onClick={()=>{sortEmployees("city")}}>City</button></th>
+                                <th><button onClick={()=>{sortEmployees("state")}}>State</button></th>
+                                <th><button onClick={()=>{sortEmployees("zip")}}>Zip Code</button></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {sortedEmployees[page].map((employee) => <TableRow employee={employee} key={sortedEmployees[page].indexOf(employee)}/>)}
+                        </tbody>
+                    </table>
+                </div>
+            </React.Fragment>
+         : <p>Employees list is empty</p>
         }
         </React.Fragment>
     )
--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -7,9 +7,9 @@ export function TableRow({employee}){
         <tr>
             <td>{employee.firstName}</td>
             <td>{employee.lastName}</td>
-            <td>{startDate.getDate() + "/ " + startDate.getMonth() + "/ " + startDate.getFullYear()}</td>
+            <td>{startDate.getDate().toString().padStart(2, 0) + "/ " + (startDate.getMonth() + 1).toString().padStart(2, 0) + "/ " + startDate.getFullYear()}</td>
             <td>{employee.department}</td>
-            <td>{birthDate.getDate() + "/ " + birthDate.getMonth() + "/ " + birthDate.getFullYear()}</td>
+            <td>{birthDate.getDate().toString().padStart(2, 0) + "/ " + (birthDate.getMonth() + 1).toString().padStart(2, 0)  + "/ " + birthDate.getFullYear()}</td>
             <td>{employee.street}</td>
             <td>{employee.city}</td>
             <td>{employee.state}</td>
