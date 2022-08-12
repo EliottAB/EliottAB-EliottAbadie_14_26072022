efe218510632036f3f807457694cb212b1be0882
Abadie Eliott <eliott.abadiesouchar@icloud.com>
2022-08-12 14:51:59 +0200

    search function without css

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -9,7 +9,7 @@ export function Table(){
     const defaultSort = "firstName"
     const employees = useSelector(state => state.employees)
 
-    const [maxEmployees, setMaxEmployees] = useState(5)
+    const [maxEmployees, setMaxEmployees] = useState(2)
     const [page, setPage] = useState(0)
     const [sortedEmployees, setSortedEmployees] = useState(()=>{
         const baseValue = _.orderBy(employees, [defaultSort], ["asc"])
@@ -17,29 +17,57 @@ export function Table(){
     })
     const [lastSort, setLastSort] = useState(defaultSort)
     
+    const searchEmployees = useCallback(()=>{
+        let searchedEmployees = []
+        let search = document.querySelector(".searchemployee").value.toLowerCase().split(" ")
+        employees.forEach(employee => {
+            //count to check if all words in searchbar are presents.
+                let countword = 0
+                search.forEach(word => {
+                    //"stop" stop the algorythm to search 2 valids values. 1 is enough.
+                    let stop = false
+                    Object.values(employee).forEach(element => {
+                        if (element.toLowerCase().includes(word) && stop === false) {
+                            countword ++
+                            stop = true
+                        }
+                    });
+                });
+                if (countword >= search.length) {
+                    searchedEmployees.push(employee)
+                }
+            });
+        return searchedEmployees
+    }, [employees])
+    
     const sortEmployees = useCallback((sort = lastSort)=>{
+
         setSortedEmployees(()=>{
-            const baseValue = _.orderBy(employees, [sort ? sort : lastSort], ["asc"])
+            let searchedEmployees = employees
+            if (document.querySelector(".searchemployee").value.length>0) {
+                searchedEmployees = searchEmployees()
+            }
+            const baseValue = _.orderBy(searchedEmployees, [sort ? sort : lastSort], ["asc"])
             setLastSort(sort)
             return _.chunk(baseValue, maxEmployees)
         })
-    }, [employees, maxEmployees, lastSort, setSortedEmployees])
+        
+    }, [employees, maxEmployees, lastSort, setSortedEmployees, searchEmployees])
     
     useEffect(()=>{
         sortEmployees()
     }, [maxEmployees, sortEmployees])
 
+
     return(
-        <React.Fragment>
-        {
-            sortedEmployees[0] ?
             <React.Fragment>
-                <select className="maxrows" onChange={(e)=>{setPage(0); setMaxEmployees(parseInt(e.target.value))}}>
+                <select className="maxrows" onChange={sortedEmployees[0] ? (e)=>{setPage(0); setMaxEmployees(parseInt(e.target.value))} : null}>
                     <option value="10">10</option>
                     <option value="25">25</option>
                     <option value="50">50</option>
                     <option value="100">100</option>
                 </select>
+                <input type="search" className="searchemployee" onChange={(e)=>{setPage(0); sortEmployees()}}/>
                 <div className="tableContainer">
                     <table className="employeelist">
                         <thead>
@@ -56,19 +84,24 @@ export function Table(){
                             </tr>
                         </thead>
                         <tbody>
-                            {sortedEmployees[page].map((employee) => <TableRow employee={employee} key={sortedEmployees[page].indexOf(employee)}/>)}
+                        {
+                        sortedEmployees[0] ?
+                            sortedEmployees[page].map((employee) => <TableRow employee={employee} key={sortedEmployees[page].indexOf(employee)}/>)
+                        :
+                            <tr className="emptylist">
+                                <td>Employees list is empty</td>
+                                <td></td>
+                            </tr>
+                        }
                         </tbody>
                     </table>
                 </div>
                 <div className="pagination">
-                    <button className="prevpage" onClick={()=>{setPage((page)=> page > 0 ? page - 1 : 0)}}>-</button>
-                    <p>{page+1}/{sortedEmployees.length}</p>
-                    <button className="nextpage" onClick={()=>{setPage((page)=> page+1 < sortedEmployees.length ? page + 1 : sortedEmployees.length - 1)}}>+</button>
+                    <button className="prevpage" onClick={()=>{setPage((page)=> page > 0 ? page - 1 : 0)}}/>
+                    <p>{sortedEmployees[0] ? page+1 : 0}/{sortedEmployees.length}</p>
+                    <button className="nextpage" onClick={()=>{setPage((page)=> page+1 < sortedEmployees.length ? page + 1 : sortedEmployees.length - 1)}}/>
                 </div>
             </React.Fragment>
-         : <p>Employees list is empty</p>
-        }
-        </React.Fragment>
     )
 
 }
